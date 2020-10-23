f3e5a4d904b599e8a935dc7adac716d706e15128
Matthew Oballe <oballematt@gmail.com>
2020-10-22 23:10:47 -0500

    updating file structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const directory = path.join(__dirname, "/public");
 
 
