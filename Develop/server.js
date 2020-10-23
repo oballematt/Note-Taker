bda4d9b41dfd43368b7c80f642a599b7eb9b009b
Matthew Oballe <oballematt@gmail.com>
2020-10-22 20:44:08 -0500

    added all the required get routes

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,31 @@ const directory = path.join(__dirname, "/public");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+fs.readFile("db/db.json","utf8", (err, data) => {
+    if (err) throw err
+    const info = JSON.parse(data)
+
+    app.get("/api/notes", function(req, res){
+        res.sendFile(path.join(__dirname, "/db/db.json"))
+    })
+
+    app.get("/api/notes/:id", function(req, res){
+        res.json(info[req.params.id])
+    })
+
+    app.get("/notes", function(req, res){
+        res.sendFile(path.join(directory, "notes.html"))
+    })
+
+    app.get("*", function(req, res){
+        res.sendFile(path.join(directory, "index.html"))
+    })
+
+
+
+
+
+
+
+})
